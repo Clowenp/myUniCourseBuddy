1b822c54371eda015e934098e6eb6323667984e8
mateedo <mateoshkodra9@gmail.com>
2023-08-19 10:40:54 -0400

    login page front-end and styling

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
-import Image from 'next/image'
-
+// Home page = Log-in Page.
+import Header from "./components/header.js"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>myUniCourseBuddy</h1>
-    </main>
+    // (note) handle event such that new link isnt http://localhost:3000/?username=&password=
+    <div>
+      <Header />
+      <form className='login'>
+        <h1>Log-in</h1>
+        {/* <label for="username">username:</label> */}
+        <input type="text"
+               name="username"
+               placeholder="username"
+               className="login-form-textbox" />
+        {/* <label for="password">password:</label> */}
+        <input type="text" 
+               name="password"
+               placeholder="password"
+               className="login-form-textbox" />
+        <button type="submit" className='login-form-submit'>Log-in!</button>
+        <p>Don't have an account? <a href="">Register</a> here!</p>
+      </form>
+    </div>
   )
 }
