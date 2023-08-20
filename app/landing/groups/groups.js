a4afe33211d71b804a1f7630c7717a8426f392cd
David Hu <badjokesdavid@gmail.com>
2023-08-20 02:54:35 -0400

    updated group scroll

--- app/landing/groups/groups.js
+++ app/landing/groups/groups.js
@@ -19,7 +19,7 @@ export default function Groups(props){
     }
 ];
     let styles = {
-        overflowX: groupsInfo.length > 3 ? "auto" : "hidden",
+        // overflowX: groupsInfo.length > 3 ? "scroll" : "hidden",
     }
     let groups = groupsInfo.map((group) => {
         return <GroupCard key={group.name} name={group.name} members={group.members}/>
@@ -37,9 +37,12 @@ export default function Groups(props){
                 <button className="buttons" id="create-group-button" onClick={getCreate}>Create Group</button>
                 <button className="buttons" id="join-group-button" onClick={getJoin}>Join Group</button>
             </div>
-            <div className="group-cards" style={styles}>
-                {groups}
+            <div style={{overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <div className="group-cards" style={{...styles, ...{overflow:'auto'}}}>
+                  {groups}
+                </div>
             </div>
+            
 
         </div>
     )
