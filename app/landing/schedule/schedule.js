4f48c5b63a170e0e307546d3cfc4471399eb74b9
Owen P <surfo8970bb@gmail.com>
2023-08-20 04:35:52 -0400

    update calendar

--- app/landing/schedule/schedule.js
+++ app/landing/schedule/schedule.js
@@ -3,16 +3,72 @@ import './schedule.css'
 import { Inter } from 'next/font/google'
 import ScheduleRow from './scheduleRow/scheduleRow';
 export default function Schedule(){
+
+    //colours, each course should be assignment a colour
+    let colors = ["red", "orange", "green", "blue", "pink"]
+
     let rowInfo = [];
-    for(let i = 8; i <= 22; i++){
+    for(let i = 8; i <= 22; i+=0.5) {
         rowInfo.push(i)
     }
+
+    // sample data, each row
+    
+
     let rows = rowInfo.map((rowTime) => {
-        return <ScheduleRow key={rowTime} time={rowTime}/>
+        let background = []
+        let course = []
+
+        for (let i = 0; i <= 5; i++) {
+            if (i == 0) {
+                background[0] = rowTime
+            }
+            if (rowTime == 8.5 || rowTime == 9) {
+                if (i == 2 || i == 4) {
+                    background[i] = "red"
+                    if (rowTime == 8.5) {
+                        course[i] = "Math 235"
+                    }
+                }
+                
+            } else if (rowTime == 13 || rowTime == 13.5 || rowTime == 14) {
+                if (i == 2 || i == 4) {
+                    background[i] = "orange"
+                    if (rowTime == 13.5) {
+                        course[i] = "Stat 231"
+                    }
+                }
+            } else if (rowTime == 12.5 || rowTime == 12) {
+                if (i == 1 || i == 3 || i == 5) {
+                    background[i] = "green"
+                    if (rowTime == 12) {
+                        course[i] = "Math 237"
+                    }
+                }
+            } else if (rowTime == 10 || rowTime == 10.5 || rowTime == 11) {
+                if (i == 1 || i == 3 || i == 5) {
+                    background[i] = "blue"
+                    if (rowTime == 10.5) {
+                        course[i] = "CS 245"
+                    }
+                }
+            } else if (rowTime == 14.5 || rowTime == 15) {
+                if (i == 1 || i == 3 || i == 5) {
+                    background[i] = "pink"
+                    if (rowTime == 14.5) {
+                        course[i] = "CS 246"
+                    }
+                }
+            } else {
+                background[i] = ""
+            }
+        }
+
+        return <ScheduleRow key={rowTime} time={rowTime} cellColor={background} courses={course}/>
     })
     return (
         <div className="schedule">
-            <table className="schedule-table">
+            <table className="schedule-table vertical-borders">
                 <tr id="headings">
                     <th id="time-slots"></th>
                     <th>Monday</th>
@@ -21,7 +77,7 @@ export default function Schedule(){
                     <th>Thursday</th>
                     <th>Friday</th>
                 </tr>
-                {rows}  
+                {rows}
             </table>
         </div>
     )
--- app/landing/schedule/scheduleRow/scheduleRow.js
+++ app/landing/schedule/scheduleRow/scheduleRow.js
@@ -1,24 +1,70 @@
+import { redirect } from 'next/dist/server/api-utils';
 import './scheduleRow.css'
 export default function ScheduleRow(props) {
     let time = "";
-    if(props.time < 12){
-      time = `${props.time}:00 AM`;
+    let color = props.cellColor
+
+    let cellClass = []
+
+    for (let i = 1; i <= 5; i++) {
+        let cellBackground = "split-background-"
+
+        // if course one half and no course the second
+        if (true) {
+            // check for the class number
+            //  each course will either be assigned 1,2,3,4 or 5
+            if (true) {
+
+                cellBackground += "1-"
+            } else {
+
+            }
+
+            
+            // check 
+            if (true) {
+                cellBackground += "1"
+            } else {
+
+            }
+        } else if (true) { //all coloured
+
+        } else {
+
+        }
+        cellClass[i] = color[i]
     }
-    else if (props.time == 12){
-        time = "12:00 PM";
+
+    //hour
+    time = Math.trunc(props.time)
+
+    if (time > 12) {
+        time = time - 12
     }
-    else{
-        time = `${props.time-12}:00 PM`;
+    time += ":"
+
+    //minutes
+    if (props.time % 1 !== 0) {
+        time += "30"
+    } else {
+        time += "00"
+    }
+
+    if (props.time < 12) {
+        time += `AM`;
+    }
+    else {
+        time += "PM";
     }
     return(
         // <div className="schedule-row">
             <tr>
-                <td>{time}</td>
-                <td> </td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td className="half-background">{time}</td>
+                <td className={cellClass[1]}>{props.courses[1]}</td>
+                <td className={cellClass[2]}>{props.courses[2]}</td>
+                <td className={cellClass[3]}>{props.courses[3]}</td>
+                <td className={cellClass[4]}>{props.courses[4]}</td>
+                <td className={cellClass[5]}>{props.courses[5]}</td>
             </tr>
         // </div>
     )
